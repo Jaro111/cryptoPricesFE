bbba72a39e06e0fdb656c9566f24df6a85b4bf0d
Jaro111 <jarokark@protonmail.com>
2024-05-02 15:21:13 +0100

    enhancement: basic feth done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import React from "react";
+import { getRequest } from "./utils";
 import "./App.css";
 import { CryptoSpace } from "./components/CryptoSpace/CryptoSpace";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+
+  const currencies = async () => {
+    const coins = await getRequest();
+    setData(coins);
+  };
+
+  useEffect(() => {
+    currencies();
+  }, []);
 
   return (
     <>
-      <CryptoSpace />
+      <div>
+        {data.map((item, index) => {
+          return <p key={index}>{item.name}</p>;
+        })}
+      </div>
     </>
   );
 }
