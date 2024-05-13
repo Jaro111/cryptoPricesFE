0f24a4c4e90a75520f5a7c97ba1a08e12b35bced
Jaro111 <jarokark@protonmail.com>
2024-05-13 21:37:22 +0100

    enhancement: drop down menu for chains done + functions editing text

---
+++ src/components/ChainList/ChainList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./ChainList.css";
+import { cropText } from "../../../common/functionsJs";
+import { MdContentCopy } from "react-icons/md";
+
+export const ChainList = (props) => {
+  //
+  const copy = async (textToCopy) => {
+    console.log(textToCopy);
+    if ("clipboard" in navigator) {
+      await navigator.clipboard.writeText(textToCopy);
+    } else {
+      document.execCommand("copy", true, textToCopy);
+    }
+  };
+  const listOfChains = props.chains.map((item, index) => (
+    <li className="listChainContent" key={index}>
+      <p className="addressTickerCont">{item.platform.coin.symbol}</p>
+      <p className="addressCont">{cropText(item.contract_address, 5, 5)}</p>
+      <MdContentCopy
+        className="copy"
+        onClick={() => {
+          copy(item.contract_address);
+        }}
+      />
+    </li>
+  ));
+  return <ul className="chainList">{listOfChains}</ul>;
+};
---
+++ src/components/DropChain/DropChain.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+import { ChainList } from "../ChainList/ChainList";
+import "./DropChain.css";
+
+export const DropChain = (props) => {
+  const [isdropChainVisible, setDropChainVisible] = useState(false);
+
+  const chains = props.coin.meta.contractAdress;
+
+  const handleDropEnter = () => {
+    setDropChainVisible(true);
+    console.log(props.coin);
+  };
+
+  const handleDropLeave = () => {
+    setDropChainVisible(false);
+  };
+
+  return (
+    <div
+      className="chainMenu"
+      onClick={handleDropEnter}
+      onMouseLeave={handleDropLeave}
+    >
+      <button>
+        &emsp;
+        {">"}
+        &emsp;
+      </button>
+      {isdropChainVisible && <ChainList chains={chains} />}
+    </div>
+  );
+};
