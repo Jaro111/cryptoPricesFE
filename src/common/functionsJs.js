b50f5d6ccc09abb8cd4af5d7a0c0ea8d09747b17
Jaro111 <jarokark@protonmail.com>
2024-06-11 11:53:58 +0100

    clickCopy funnctions added to style

--- src/common/functionsJs.js
+++ src/common/functionsJs.js
@@ -19,4 +19,22 @@ export const cutUrl = (url) => {
     }
   }
 };
-//
+
+// copy text
+export const copy = async (textToCopy) => {
+  console.log(textToCopy);
+  if ("clipboard" in navigator) {
+    await navigator.clipboard.writeText(textToCopy);
+  } else {
+    document.execCommand("copy", true, textToCopy);
+  }
+};
+
+// change style when Copy
+
+export const clickCopyStyle = (setState, style1, style2) => {
+  setState(style1);
+  setTimeout(() => {
+    setState(style2);
+  }, 300);
+};
